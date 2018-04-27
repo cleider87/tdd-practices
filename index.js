79a06172b2bc9dd1b77a09222a2ace72498eeb16
Cleider <cleider87@gmail.com>
2018-04-26 23:55:33 -0300

    Refactor

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var config = require('./config/config');
-
+var morgan = require('morgan');
 var server=require('./server');
+
+var db;
+
+//var opbeat = require('opbeat').start({
+//    appId: 'a33e4ef166',
+//    organizationId: '1d9b808c793c4921b18827adafffc55a',
+//    secretToken: '1e2e8893353e1b58b9303e80369482034147447c'
+//})
 
 if (process.env.NODE_ENV === "test") {
     server.listen(config.test_db, function (err) {
@@ -9,10 +17,11 @@ if (process.env.NODE_ENV === "test") {
         console.log("Testing App on port " + config.test_port);
     });
 } else {
+    server.use(morgan('dev'));
     server.listen(config.port, function (err) {
         if (err)
             throw err;
         console.log("App listening on port " + config.port);
     });
 }
-
+//server.use(opbeat.middleware.express());
